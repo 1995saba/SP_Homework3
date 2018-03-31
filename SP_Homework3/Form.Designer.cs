d1c4bfe88808a67f5903d65fa7b97633966bf524
Sabina <sabina.orunbaeva@gmail.com>
2018-03-31 17:20:33 +0600

    Add files via upload

---
+++ SP_Homework3/Form.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SP_Homework3
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encryptRadioButton = new System.Windows.Forms.RadioButton();
+            this.decryptRadioButton = new System.Windows.Forms.RadioButton();
+            this.startButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // encryptRadioButton
+            // 
+            this.encryptRadioButton.AutoSize = true;
+            this.encryptRadioButton.Location = new System.Drawing.Point(219, 71);
+            this.encryptRadioButton.Name = "encryptRadioButton";
+            this.encryptRadioButton.Size = new System.Drawing.Size(95, 17);
+            this.encryptRadioButton.TabIndex = 0;
+            this.encryptRadioButton.TabStop = true;
+            this.encryptRadioButton.Text = "Зашифровать";
+            this.encryptRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // decryptRadioButton
+            // 
+            this.decryptRadioButton.AutoSize = true;
+            this.decryptRadioButton.Location = new System.Drawing.Point(319, 71);
+            this.decryptRadioButton.Name = "decryptRadioButton";
+            this.decryptRadioButton.Size = new System.Drawing.Size(101, 17);
+            this.decryptRadioButton.TabIndex = 1;
+            this.decryptRadioButton.TabStop = true;
+            this.decryptRadioButton.Text = "Расшифровать";
+            this.decryptRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(239, 173);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Пуск";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(345, 173);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Location = new System.Drawing.Point(16, 13);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(52, 23);
+            this.chooseFileButton.TabIndex = 4;
+            this.chooseFileButton.Text = "Файл";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.ChooseFileButton_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(84, 15);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(336, 20);
+            this.filePathTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Пароль";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(84, 68);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(125, 20);
+            this.passwordTextBox.TabIndex = 7;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 130);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(404, 23);
+            this.progressBar.TabIndex = 8;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 212);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.chooseFileButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.decryptRadioButton);
+            this.Controls.Add(this.encryptRadioButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form";
+            this.Text = "Шифрование файла";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton encryptRadioButton;
+        private System.Windows.Forms.RadioButton decryptRadioButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ SP_Homework3/Form.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_Homework3
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        private Thread thread;
+        public Form()
+        {
+            InitializeComponent();
+            progressBar.Visible = false;
+        }
+
+        private void ChooseFileButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            filePathTextBox.Text = openFileDialog.FileName;
+        }
+
+        private void Encrypt()
+        {
+            byte password;
+            Byte.TryParse(passwordTextBox.Text, out password);
+            try
+            {
+                Crypt(password);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Decrypt()
+        {
+            byte password;
+            Byte.TryParse(passwordTextBox.Text, out password);
+            try
+            {
+                Crypt(password);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Crypt(byte password)
+        {
+            using (var encryptFile = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.ReadWrite))
+            {
+                while (encryptFile.Position < encryptFile.Length)
+                {
+                    byte[] array = new byte[encryptFile.Length];
+                    encryptFile.Read(array, 0, array.Length);
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        array[i] ^= password;
+                        encryptFile.Seek(0, SeekOrigin.Begin);
+                        encryptFile.Write(array, 0, array.Length);
+                        progressBar.Invoke(new Action(() => progressBar.Increment((int)encryptFile.Length)));
+                    }
+
+                    progressBar.Invoke(new Action(() => progressBar.Value = 0));
+                }
+            }
+        }
+        
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (passwordTextBox == null)
+            {
+                MessageBox.Show("Не введен пароль для шифрования!");
+            }
+            if (filePathTextBox.Text != null)
+            {
+                progressBar.Visible = true;
+                if (encryptRadioButton.Checked)
+                {
+                    try
+                    {
+                        thread = new Thread(Encrypt);
+                        thread.Start();
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("Выбран неверный путь файла или отказано в доступе!\n"
+                            +ex.Message);
+                    }
+                    MessageBox.Show("Файл успешно зашифрован!");
+                }
+                else if (decryptRadioButton.Checked)
+                {
+                    try
+                    {
+                        thread = new Thread(Decrypt);
+                        thread.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Выбран неверный путь файла или отказано в доступе!\n"
+                            + ex.Message);
+                    }
+                    MessageBox.Show("Файл успешно расшифрован!");
+                }
+                else
+                {
+                    MessageBox.Show("Не выбрано действие над файлом!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл не выбран!");
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            if (thread.IsAlive)
+            {
+                thread.Abort();
+            }
+        }
+    }
+}
